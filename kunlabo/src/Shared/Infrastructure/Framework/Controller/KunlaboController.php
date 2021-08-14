f83fce26b48a9aa45de0de0834098c99d47e98d3
pulmunyi <pol.munoz@salle.url.edu>
2021-08-15 00:19:17 +0200

    Small refactor

--- kunlabo/src/Shared/Infrastructure/Framework/Controller/KunlaboController.php
+++ kunlabo/src/Shared/Infrastructure/Framework/Controller/KunlaboController.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace Kunlabo\Home\Infrastructure\Framework\Controller;
+namespace Kunlabo\Shared\Infrastructure\Framework\Controller;
 
 use Kunlabo\User\Infrastructure\Framework\Auth\AuthUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class HomeController extends AbstractController
+final class KunlaboController extends AbstractController
 {
     #[Route('/home', name: 'web_home', methods: ['GET'])]
     public function home(): Response
     {
         $this->denyAccessUnlessGranted(AuthUser::ROLE_USER);
-        return $this->render("home/home.html.twig");
+        return $this->render("app/home.html.twig");
     }
 
     #[Route('/', name: 'web_landing', methods: ['GET'])]
     public function landing(): Response
     {
-        return $this->render("home/landing.html.twig");
+        return $this->render("landing.html.twig");
     }
 }
