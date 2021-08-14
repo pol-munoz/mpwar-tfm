fccdc8beffe2fc124361d2d88841ff09f1de37d4
pulmunyi <pol.munoz@salle.url.edu>
2021-08-15 00:23:49 +0200

    Small landing/home templates + webpack refactor

---
+++ kunlabo/frontend/assets/landing.js
@@ -0,0 +1,7 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/landing.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
--- kunlabo/frontend/webpack.config.js
+++ kunlabo/frontend/webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('auth', './assets/auth.js')
+    .addEntry('landing', './assets/landing.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
