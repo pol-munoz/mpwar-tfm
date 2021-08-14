d36c8c22c98763595d76ec9f2e2dea7c3498e77d
pulmunyi <pol.munoz@salle.url.edu>
2021-08-15 01:08:14 +0200

    Added actual check before submitting register form Other adjustments to register process

--- kunlabo/frontend/assets/controllers/register_controller.js
+++ kunlabo/frontend/assets/controllers/register_controller.js
@@ -4,29 +4,29 @@ export default class extends Controller {
     static targets = [ "name", "nameError", "email", "emailError", "password", "passwordError" ]
 
     initialize() {
-        this.nameError = false
-        this.emailError = false
-        this.passwordError = false
-        this.passwordUpper = true
-        this.passwordNumber = true
-        this.passwordLower = true
-        this.passwordSpecial = true
+        this.nameError = true
+        this.emailError = true
+        this.passwordError = true
+
+        this.passwordUpper = false
+        this.passwordNumber = false
+        this.passwordLower = false
+        this.passwordSpecial = false
     }
 
     nameChanged() {
-        if (!this.nameError && this.nameTarget.value.length > 255) {
+        if (this.nameTarget.value.length > 255) {
             this.nameError = true
             this.nameErrorTarget.parentNode.style = "opacity: 1"
             this.nameErrorTarget.innerText = "Must not exceed 255 characters"
-        }
-        if (this.nameError && this.nameTarget.value.length < 255) {
+        } else {
             this.nameError = false
             this.nameErrorTarget.parentNode.style = ""
         }
     }
 
     nameBlur() {
-        if (!this.nameError && this.nameTarget.value.length === 0) {
+        if (this.nameTarget.value.length === 0) {
             this.nameError = true
             this.nameErrorTarget.parentNode.style = "opacity: 1"
             this.nameErrorTarget.innerText = "Required"
@@ -34,14 +34,18 @@ export default class extends Controller {
     }
 
     emailChanged() {
-        if (this.emailError && this.emailTarget.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+        if (this.emailTarget.value.length > 255) {
+            this.emailError = true
+            this.emailErrorTarget.parentNode.style = "opacity: 1"
+            this.emailErrorTarget.innerText = "Must not exceed 255 characters"
+        } else if (this.emailTarget.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
             this.emailError = false
             this.emailErrorTarget.parentNode.style = ""
         }
     }
 
     emailBlur() {
-        if (!this.emailError && this.emailTarget.value.length === 0) {
+        if (this.emailTarget.value.length === 0) {
             this.emailError = true
             this.emailErrorTarget.parentNode.style = "opacity: 1"
             this.emailErrorTarget.innerText = "Required"
@@ -53,51 +57,48 @@ export default class extends Controller {
     }
 
     passwordChanged() {
-        if (!this.passwordError && this.passwordTarget.value.length > 72) {
+        if (this.passwordTarget.value.length > 72) {
             this.passwordError = true
             this.passwordErrorTarget.parentNode.style = "opacity: 1"
             this.passwordErrorTarget.innerText = "Must not exceed 72 characters"
-        }
-        if (this.passwordError && this.passwordTarget.value.length < 72) {
+        } else {
             this.passwordError = false
             this.passwordErrorTarget.parentNode.style = ""
         }
-
-        if (!this.passwordLower && this.passwordTarget.value.match(/[a-z]/g)) {
-            this.passwordLower = true
-            this.showPasswordError()
+        if (this.passwordTarget.value.length > 0) {
+            if (this.passwordTarget.value.length < 8) {
+                this.passwordError = true
+                this.passwordErrorTarget.parentNode.style = "opacity: 1"
+                this.passwordErrorTarget.innerText = "Must be at least 8 characters long"
+            } else {
+                this.showPasswordError()
+            }
+        } else {
+            this.passwordError = false
+            this.passwordErrorTarget.parentNode.style = ""
         }
-        if (!this.passwordUpper && this.passwordTarget.value.match(/[A-Z]/g)) {
-            this.passwordUpper = true
-            this.showPasswordError()
-        }
-        if (!this.passwordNumber && this.passwordTarget.value.match(/[0-9]/g)) {
-            this.passwordNumber = true
-            this.showPasswordError()
-        }
-        if (!this.passwordSpecial && this.passwordTarget.value.match(/[^\w]/g)) {
-            this.passwordSpecial = true
-            this.showPasswordError()
-        }
-
     }
 
     passwordBlur() {
-        if (!this.passwordError && this.passwordTarget.value.length === 0) {
+        if (this.passwordTarget.value.length === 0) {
             this.passwordError = true
             this.passwordErrorTarget.parentNode.style = "opacity: 1"
             this.passwordErrorTarget.innerText = "Required"
+        } else if (this.passwordTarget.value.length < 8) {
+            this.passwordError = true
+            this.passwordErrorTarget.parentNode.style = "opacity: 1"
+            this.passwordErrorTarget.innerText = "Must be at least 8 characters long"
         } else {
-            this.passwordLower = this.passwordTarget.value.match(/[a-z]/g)
-            this.passwordUpper = this.passwordTarget.value.match(/[A-Z]/g)
-            this.passwordNumber = this.passwordTarget.value.match(/[0-9]/g)
-            this.passwordSpecial = this.passwordTarget.value.match(/[^\w]/g)
-
             this.showPasswordError()
         }
     }
 
     showPasswordError() {
+        this.passwordLower = this.passwordTarget.value.match(/[a-z]/g)
+        this.passwordUpper = this.passwordTarget.value.match(/[A-Z]/g)
+        this.passwordNumber = this.passwordTarget.value.match(/[0-9]/g)
+        this.passwordSpecial = this.passwordTarget.value.match(/[^\w]/g)
+
         let error = ""
 
         if (!this.passwordLower) {
@@ -120,6 +121,17 @@ export default class extends Controller {
             this.passwordErrorTarget.innerText = error
         } else {
             this.passwordErrorTarget.parentNode.style = "opacity: 0"
+        }
+    }
+
+    submit(event) {
+        let passwordOk = this.passwordLower && this.passwordUpper && this.passwordNumber && this.passwordSpecial
+
+        if (this.nameError || this.emailError || this.passwordError || !passwordOk) {
+            event.preventDefault()
+            this.nameBlur()
+            this.emailBlur()
+            this.passwordBlur()
         }
     }
 }
