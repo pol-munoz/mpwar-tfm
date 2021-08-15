b3b355f94889e71cb05c1321fde24a20bc4eae7c
pulmunyi <pol.munoz@salle.url.edu>
2021-08-15 19:31:00 +0200

    Small frontend tweaks to allow scroll in the sidebar

--- kunlabo/frontend/assets/controllers/sidebar_controller.js
+++ kunlabo/frontend/assets/controllers/sidebar_controller.js
@@ -4,10 +4,11 @@ export default class extends Controller {
     static targets = [ "nav", "icon" ]
 
     initialize() {
+        this.children = this.navTarget.getElementsByClassName('App-nav-item')
         this.isOpen = (localStorage.getItem('openSidebar') ?? 'true') === 'true'
         this.renderSidebar()
         window.setTimeout(() => {
-            this.navTarget.classList.add('App-nav-animated')
+            this.children.forEach(item => item.classList.add('App-nav-item-animated'))
         }, 0)
     }
 
@@ -19,10 +20,10 @@ export default class extends Controller {
 
     renderSidebar() {
         if (this.isOpen) {
-            this.navTarget.style = "width: 150px"
+            this.children.forEach(item => item.style = "width: 150px")
             this.iconTarget.classList.replace('fa-angle-double-right', 'fa-angle-double-left')
         } else {
-            this.navTarget.style = ""
+            this.children.forEach(item => item.style = "")
             this.iconTarget.classList.replace('fa-angle-double-left', 'fa-angle-double-right')
         }
     }
